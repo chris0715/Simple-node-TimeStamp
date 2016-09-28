283aea1a5fa269fd6c1e0818a5233456db880aea
Chris <christopher.encarnacion@gmail.com>
2016-09-28 04:18:12 +0000

    tu sabe

--- server.js
+++ server.js
@@ -1,10 +1,61 @@
 var express = require('express');
 var app = express();
+var url = require("url")
+app.get('/', function (req,res) 
+{
+    res.sendFile(__dirname+'/index.html')
+    
+})
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get('/:id', function(req,res)
+{
+    
+    //console.log(entrada)
+    var espaciador = /\s/;
+    var direcion = req.param('id');
+    //console.log(direcion)
+    
+    var natural, unix = "";
+    
+    
+     if(espaciador.test(direcion))
+     {
+        
+         var klk = new Date(direcion.split(' ').join());
+         natural = direcion;
+         unix = convertirUnix(klk);
+     }
+     
+  
+   else
+   {
+       unix = direcion
+       natural = convertirNatural(direcion)
+    
+   }
+   console.log(natural);
+    
+    if( natural!=="Invalid Date" && unix!=='Invalid Date')
+        res.json({"unix": unix, "natural": natural})
+        
+        else{
+            res.json({"unix": null, "natural": null})
+        }
+})
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
-});
+
+
+var convertirUnix = function(objectoFecha)
+{
+    var date = new Date(objectoFecha.toDateString())
+    return date.getTime()/1000;
+}
+
+var convertirNatural = function(tomaString)
+{
+    return new Date(tomaString* 1000).toDateString()
+    
+    
+}
+
+app.listen(8080)
